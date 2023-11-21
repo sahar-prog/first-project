a36b82d29bdb5c29aa4b8d7bb74d9505e0be25a8
Sahar <saharrroukassi@gmail.com>
2023-11-21 19:53:20 +0100

    Adding prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first-project';
+
+  constructor(private http: HttpClient, private route: Router) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<any[]>('https://wizard-world-api.herokuapp.com/Elixirs')
+      .pipe(
+        map((value) =>
+          value.map((a) => {
+            return { id: a.id, name: a.name };
+          }),
+        ),
+      )
+      .subscribe((result) => console.log(result));
+  }
 }
