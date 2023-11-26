5569af57d577c8b104d7a00d50ba67990c16b717
Sahar <saharrroukassi@gmail.com>
2023-11-26 13:06:22 +0100

    unsubscribe ElixerService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, effect } from '@angular/core';
 import { ElixerService } from './elixer.service';
 import { Elixer } from './elixer.model';
+import { map, Observable, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'first-project';
-  elixersList!: Elixer[];
+  elixersList: Elixer[] = [];
+  elixer!: Elixer;
+  subscription!: Subscription;
+
   constructor(private elixerSer: ElixerService) {}
 
   ngOnInit() {
-    this.elixerSer.getAllElixer().subscribe((data) => {
-      this.elixersList = data;
-      console.log(this.elixersList);
-    });
+    this.subscription = this.elixerSer.getAllElixer().subscribe(
+      (elixer) =>
+        (this.elixersList = elixer.map(
+          (value) =>
+            (this.elixer = {
+              name: value.name,
+              effect: value.effect,
+              sideEffects: value.sideEffects,
+              caracteristics: value.caracteristics,
+              time: value.time,
+              dificulty: value.dificulty,
+            }),
+        )),
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe;
   }
 }
